cb9da0eef3229c9f5fbb3787f03b60e35b184c24
ChaitanyaHazarika <hiranya935@gmail.com>
2024-02-23 19:51:09 +0530

    first commit

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 const express = require('express');
-const { initializeApp, database } = require('firebase-admin');
-const database = require('firebase/database')
-const app_express = express()
+const app  = express()
+app.use(express.static('public'))
 
-// Import the functions you need from the SDKs you need
-const firebaseConfig = {
-    apiKey: "AIzaSyCkDL6o5zIadycIqJoROr48mqUB3EkZab0",
-    authDomain: "mikutoys-d47de.firebaseapp.com",
-    databaseURL: "https://mikutoys-d47de-default-rtdb.firebaseio.com",
-    projectId: "mikutoys-d47de",
-    storageBucket: "mikutoys-d47de.appspot.com",
-    messagingSenderId: "453934610008",
-    appId: "1:453934610008:web:a2f61888e5672d01773c2d"
-  };
-  
-  // Initialize Firebase
-const app = initializeApp('')
-// Initialize Firebase
-var db = database()
-
-
-
-const dbRef = 
-app_express.use(express.static('public'))
-
-app_express.listen(4000,() => {
+app.listen(4000,() => {
     console.log("Listening on 4000");
 })
