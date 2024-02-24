cb99179e273fcb791751d06168ebdb37540bc5ec
Hazzu200 <88277431+ChaitanyaHazarika@users.noreply.github.com>
2024-02-24 11:55:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app  = express()
 
 
 app.use(express.static('public'))
-
-app.listen(4000,() => {
+const PORT  = process.env.PORT || 4000
+app.listen(PORT,() => {
     console.log("Listening on 4000");
 })
